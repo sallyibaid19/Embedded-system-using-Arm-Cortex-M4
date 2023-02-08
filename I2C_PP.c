3dc48b5ff414c27c6ea1a6db0da63a909027459a
Sally G. Ibaid <56202060+SallyGhazi@users.noreply.github.com>
2023-02-08 15:59:08 +0200

    Add files via upload

---
+++ I2C_PP.c
@@ -0,0 +1,15 @@
+What is I2C?
+الاختصار : it is acronym of Inter-Integrated Circuit.
+- it also called Two-Wire Interface.
+عبارة عن سلكين احدهما للداتا والاخر للclock
+characteristics:
+- Synchronous
+- Half-Duplex
+- Multi-Master
+- Used to connect lower speed ICs to MCUs
+- Used for short distance communications
+- Open-Collect or Open-Drain bus.
+
+/*
+
+*/
